c78fcdda2fc913605663b46cf5c7ea8c5bdcffc0
Sevan <sevan_babaxanloo@yahoo.com>
2026-04-29 13:53:36 +0400

    fav icon change

--- app/layout.js
+++ app/layout.js
@@ -14,6 +14,9 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Sevan Portfolio",
   description: "Frontend developer portfolio",
+  icons: {
+    icon: '/fav.png',
+  },
 };
 
 export default function RootLayout({ children }) {
